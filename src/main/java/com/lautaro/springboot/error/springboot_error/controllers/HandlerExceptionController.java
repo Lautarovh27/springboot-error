b5482e1106947f44fd9662c902836af4af262ce1
Lautaro <lautarovanh@gmail.com>
2026-06-07 16:12:59 -0300

    Se agrego el number format exception

--- src/main/java/com/lautaro/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/lautaro/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,11 +1,15 @@
 package com.lautaro.springboot.error.springboot_error.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.lautaro.springboot.error.springboot_error.models.Error;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -24,6 +28,17 @@ public class HandlerExceptionController {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> numberFormatException(Exception ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("date", new Date().toString());
+        error.put("error", "Number format exception, invalid number");
+        error.put("message", ex.getMessage());
+        error.put("status", String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+        return error;
+    }
+
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<Error> notFound(NoHandlerFoundException ex) {
         Error error = new Error();
@@ -33,6 +48,7 @@ public class HandlerExceptionController {
         error.setStatus(HttpStatus.NOT_FOUND.value());
 
         return ResponseEntity.status(404).body(error);
-        
     }
+
+
 }
